44f3678cb008b4771cf88a92a92a4bceb077af2e
Kshitij <m.krishna0513@gmail.com>
2026-03-02 23:14:30 +0530

    Order and OrderItem models created

---
+++ src/main/java/com/kk/SpringMVCproject/model/OrderItem.java
@@ -0,0 +1,32 @@
+package com.kk.SpringMVCproject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+
+    @Id
+    private int id;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+    BigDecimal totalPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+}
